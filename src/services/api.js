e1a5d08e0361cc9c7a31b2635ad243092773987e
Jordi <jordicapde1@gmail.com>
2026-02-19 10:06:09 +0100

    Refactor of api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,82 +3,72 @@ import { crearDetallEstat } from "../models/DetallEstat.js";
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbyA0pDGfmI7G_5KkZYE2Fp0c-O11KnFGyouNspe_nGH1dt5aysx5Vh3zqjzfgz_-tPm/exec'
 
-export const getJocs = async () => {
-  try {
-    const response = await fetch(`${API_URL}?action=getJocs`);
-    const json = await response.json();
+// --- GET ---
+const doGet = async (action) => {
+    try {
+      const response = await fetch(`${API_URL}?action=${action}`);
+      const json = await response.json();
 
-    if (json.status === "success") {
-      return json.data.map(item => crearJoc(item));
-    } else {
-      throw new Error(json.message || "Error desconegut");
+      if (json.status === "success") {
+        return json.data;
+      } else {
+        throw new Error(json.message || "Error desconegut");
+      }
+    } catch (error) {
+      console.error(`Error obtenint ${action}:`, error);
+      throw error;
     }
-  } catch (error) {
-    console.error("Error obtenint jocs:", error);
-    throw error;
-  }
-};
+}
 
-export const getEstats = async () => {
-  try {
-    const response = await fetch(`${API_URL}?action=getEstats`);
-    const json = await response.json();
-
-    if (json.status === "success") {
-      return json.data.map(item => crearDetallEstat(item));
-    } else {
-      throw new Error(json.message || "Error desconegut");
-    }
-  } catch (error) {
-    console.error("Error obtenint préstecs:", error);
-    throw error;
-  }
-};
-
-export const postPrestec = async (nomSoci, idsJocs) => {
-  const request = {
-    action: 'postPrestec',
-    nomSoci: nomSoci,
-    idsJocs: idsJocs
-  };
-
+// --- POST ---
+const doPost = async (request) => {
   try {
     const response = await fetch(API_URL, {
       method: 'POST',
-      mode: 'no-cors',
+      // mode: 'no-cors',
       body: JSON.stringify(request),
       headers: {
         "Content-Type": "text/plain;charset=utf-8",
       },
     });
 
-    // return await response.json();
-    return { status: "succes" }
+    const json = await response.json();
+
+    if (json.status === "success") {
+      return json.data;
+    } else {
+      throw new Error(json.message || "Error desconegut");
+    }
 
   } catch (error) {
-    console.error("Error guardant préstec:", error);
+    console.error(`Error executant ${action}:`, error);
     throw error;
   }
+}
+
+//////////////////////////////////////////////////////////
+
+export const getJocs = async () => {
+  return doGet('getJocs')
+      .then(data => data.map(item => crearJoc(item)));
+};
+
+export const getEstats = async () => {
+  return doGet('getEstats')
+      .then(data => data.map(item => crearDetallEstat(item)));
+};
+
+export const postPrestec = async (nomSoci, idsJocs) => {
+  return doPost({
+    action: 'postPrestec',
+    nomSoci: nomSoci,
+    idsJocs: idsJocs
+  });
 };
 
 export const actualitarPrestec = async (idsPrestecs) => {
-  try {
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify({
-        action: 'putPrestec',
-        idsPrestecs: idsPrestecs
-      }),
-      headers: { "Content-Type": "text/plain;charset=utf-8" },
-    });
-
-    const resultat = await response.json();
-    if (resultat.status === 'error') throw new Error(resultat.message);
-
-    return true;
-
-  } catch (error) {
-    console.error("Error retornant jocs:", error);
-    throw error;
-  }
+  return doPost({
+    action: 'putPrestec',
+    idsPrestecs: idsPrestecs
+  });
 };
